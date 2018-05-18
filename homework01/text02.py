b6f5b261fae0cec175fc2b33f64bd0cf27323491
nykh2010 <466721896@qq.com>
2018-05-18 10:31:09 +0800

    使用多线程遍历目标文件夹下的所有目录和文件
    使用多线程遍历目标文件夹下的所有目录和文件

--- homework01/text02.py
+++ homework01/text02.py
@@ -1,48 +1,69 @@
-# -*- coding: utf-8 -*-
-'''
-    有一个文件夹，下面有1000+文件。把这个文件夹中的所有文件拷贝到另一个文件夹下：
-    要求：使用多进程（多线程）或进程池（线程池）；使用Hash证明文件完整性
-'''
 import threading
 import os
 
-PATH = "H:\Redirect\Documents"
+ROOT_PATH = "C:\\Users\\xulingfeng\\Documents\\"
+ROOT_NAME = "Tencent Files"
+DST_PATH = "D:\\"
+search_path = []
+copy_path = []
+thread_list = []
 
-SRC_FOLDER_PATH = "Source Insight 4.0\\"
-DST_FOLDER_PATH = "Source Insight 4.0 BACKUP\\"
+def copy_to_dst_path(copy_path):
+	while len(copy_path) != []:
+		dir = copy_path.pop()
+		with open(dir,'rb') as src_file:
+			with open(DST_PATH+dir,'a') as dst_file:
+				flag = True
+				while flag:
+					try:
+						buf = src_file.read(4096)
+						dst_file.write(buf)
+					except EOFError:
+						src_file.close()
+						dst_file.close()
+						flag = False
 
-def file_list(path):
-    pass
+def get_from_search_path(search_path,copy_path):
+	dir = search_path.pop()
+	listdirs = os.listdir(ROOT_PATH+dir)
+	for listdir in listdirs:
+		new_path = dir + '\\' + listdir
+		# print(new_path)
+		if os.path.isdir(ROOT_PATH+dir+'\\'+listdir):
+			search_path.append(new_path)
+		else:
+			copy_path.append(new_path)
+		# print(listdir)
+		# pass
 
-def move(dst,src):
-    pass
-    
+def main():
+	os.chdir(ROOT_PATH)
+	# os.mkdir(DST_PATH)
+	print(os.listdir())
+	search_path.append(ROOT_NAME)
+	while True:
+		if search_path == []:
+			break
+		else:
+			thread_count = len(search_path)
+			# print(thread_count)
+			for i in range(thread_count):
+				main_thread = threading.Thread(target=get_from_search_path,args=(search_path,copy_path))
+				thread_list.append(main_thread)
+				main_thread.start()
+			for thread in thread_list:
+				thread.join()
+	for i in range(5):
+		thread = threading.Thread(target=copy_to_dst_path,args=(copy_path,))
+		thread_list.append(thread)
+		thread.start()
+	for thread in thread_list:
+		thread.join()
+
+	print(search_path)
+	print(copy_path)
+	print(len(copy_path))
+
+
 if __name__ == '__main__':
-    os.chdir(PATH)
-    try:
-        os.mkdir(DST_FOLDER_PATH)
-    except FileExistsError:
-        print(DST_FOLDER_PATH,"exist")
-#    os.chdir(PATH+SRC_FOLDER_PATH)
-    files = os.listdir(SRC_FOLDER_PATH)
-#    print(files)
-    for file in files:
-        print(file)
-        if os.path.isdir(SRC_FOLDER_PATH+file):
-            print(DST_FOLDER_PATH+file)
-            try:
-                os.mkdir(DST_FOLDER_PATH+file)
-            except FileExistsError:
-                continue
-        else:
-            try:
-                src_file = open(SRC_FOLDER_PATH+file,"rb")
-            except:
-                print("open failed:"+file)
-                continue
-            try:
-                dst_file = open(DST_FOLDER_PATH+file,"a")
-            except FileExistsError:
-                print("file exist:"+file)
-            dst_file.close()
-            src_file.close()
+	main()
